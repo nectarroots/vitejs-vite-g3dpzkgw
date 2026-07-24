8c46fe97d6142c7ddc9c04634aa452ebc44689a6
nectarroots <nectarrootsllp@gmail.com>
2026-07-24 12:42:59 +0530

    Secured API Keys

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,7 @@
+// Updating keys for security
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://wxfrqcmzwdmxrfrxyupp.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind4ZnJxY216d2RteHJmcnh5dXBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ3MzY2MzMsImV4cCI6MjEwMDMxMjYzM30.bPufX5GoDQ2X2Q7e5raRo7nsVqP4edteNxSF8AVeJOU';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
